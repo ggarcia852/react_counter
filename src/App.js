f176f639a42e5924aab046ded47ed3c511c43370
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-12-02 18:01:14 -0700

    function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import react from "react";
 import Counter from "./components/Counter";
+import CounterHooks from "./components/CounterHooks";
 
 
 function App() {
   return (
+    <>
+    Counter
     <Counter initialCount={0} />
+    Counter Hooks
+    <CounterHooks initialCount={0}/>
+    </>
   )
 }
 
